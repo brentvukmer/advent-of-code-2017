39e944f31c9bd628738b412eb0a1d1acaf68f5f8
Brent Vukmer <brent.vukmer@gmail.com>
2017-12-21 07:50:34 -0500

    Solved Day 4 Part 2

--- src/advent_of_code_2017/day4.clj
+++ src/advent_of_code_2017/day4.clj
@@ -7,4 +7,20 @@
 
 (def day4-inputs (map #(clojure.string/split % #" ") (clojure.string/split-lines (clojure.string/trim (slurp (io/resource "day4"))))))
 
-(def answer-part1 (count (filter #(= (count %) (count (set %))) day4-inputs)))
+(defn no-duplicate-words
+  "docstring"
+  [passphrase]
+  (= (count passphrase) (count (set passphrase))))
+
+(def answer-part1 (count (filter #(no-duplicate-words %) day4-inputs)))
+
+;
+; Part 2
+;
+
+(defn no-anagrams
+  ""
+  [passphrase]
+  (= (count passphrase) (count (set (map set passphrase)))))
+
+(def answer-part2 (count (filter #(no-anagrams %) (filter #(no-duplicate-words %) day4-inputs))))
