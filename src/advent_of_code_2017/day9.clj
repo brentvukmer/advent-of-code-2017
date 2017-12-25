c24a049471696070cbe5fd8c35c7cc5e67d2865d
Brent Vukmer <brent.vukmer@gmail.com>
2017-12-25 13:33:34 -0500

    Setup for Day 9: initial EBNF grammar for playing in the REPL

--- src/advent_of_code_2017/day9.clj
+++ src/advent_of_code_2017/day9.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code-2017.day9
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [instaparse.core :as insta]))
 
 ;
 ; Part 1
@@ -8,10 +9,12 @@
 (defn read-lines-classpath-resource
   ""
   [name]
-  (clojure.string/split-lines (slurp (io/resource name))))
+  (slurp (io/resource name)))
 
 (def day9-inputs
-  (map #(clojure.string/split % #"\s") (read-lines-classpath-resource "day9")))
+  (read-lines-classpath-resource "day9"))
 
 (def day9-test-inputs
-  (map #(clojure.string/split % #"\s") (clojure.string/split-lines "")))
+  (clojure.string/split-lines ""))
+
+
