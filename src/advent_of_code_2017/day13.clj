59a0439cb7d39ab9cdefdff224eceb775007b23e
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-01 13:55:31 -0500

    Day 13 Part 1, all in a muddle

--- src/advent_of_code_2017/day13.clj
+++ src/advent_of_code_2017/day13.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2017.day13
   (:require [clojure.java.io :as io]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.set :as set]))
 
 ;
 ; Part 1
@@ -9,14 +10,14 @@
 (def day13-input-str
   (slurp (io/resource "day13")))
 
-(defn line-tokens->map
+(defn line-tokens->map-entry
   ""
   [[key-str val-str]]
-  (let [key (Integer/parseInt (string/trim key-str))
-        val (Integer/parseInt (clojure.string/trim val-str) )]
-    {:depth key :range val}))
+  (let [depth (Integer/parseInt (string/trim key-str))
+        range (Integer/parseInt (clojure.string/trim val-str))]
+    [depth range]))
 
-(defn lines->tokens
+(defn input->line-tokens
   ""
   [input-str]
   (map #(string/split % #":")
@@ -25,23 +26,66 @@
 (defn input->layers
   ""
   [input-str]
-  (let [tokens (lines->tokens input-str)]
-    (vec (map line-tokens->map tokens))))
+  (let [tokens (input->line-tokens input-str)
+        layers-from-input (into {} (map line-tokens->map-entry tokens))
+        input-depths (map first layers-from-input)
+        all-depths (range
+                     0
+                     (inc (last input-depths)))
+        missing-depths (set/difference
+                         (set all-depths)
+                         (set input-depths))
+        missing-layers (into {} (map vector missing-depths (repeat 0)))
+        merged-layers (merge layers-from-input missing-layers)
+        layers-as-mapsv (sort-by :depth
+                                 (map #(hash-map :depth (first %) :range (second %)) merged-layers))]
+    layers-as-mapsv))
 
-(def day13-layers
-  (input->layers day13-input-str))
-
-(defn scanner-pos
+(defn layer-scanner-pos
   ""
   [t layer]
   (let [range (:range layer)
-        pos (mod t range)]
-    (assoc layer :scanner-pos pos)))
+        pos (if (zero? range)
+              -1
+              (mod t range))]
+    (assoc layer :scanner-pos pos :time t)))
 
-(defn scanner-positions
+;
+; Cheesy, but the way to get the starting positions is via t = -1.
+;
+(defn layer-scanner-positions-after-t
   ""
   [t layers]
-  (map #(scanner-pos t %) layers))
+  (map #(layer-scanner-pos t %) layers))
 
+(defn layer-scanner-positions-seq
+  ""
+  [layers]
+  (map #(layer-scanner-positions-after-t % layers)
+       (range 0 (count layers))))
 
+(defn scanner-position-severity
+  ""
+  [layer-scanner-position]
+  (let [scanner-pos (:scanner-pos layer-scanner-position)
+        severity (* (:depth layer-scanner-position) (:range layer-scanner-position))]
+    (if (= 0 scanner-pos)
+      severity
+      0)))
+
+(defn packet-positions
+  ""
+  [trip-seq]
+  (let [indexes (range 0 (count (first trip-seq)))]
+    (map #(get (vec %1) %2) trip-seq indexes)))
+
+(defn trip-severity
+  ""
+  [trip-seq]
+  (apply + (map scanner-position-severity (packet-positions trip-seq))))
+
+(comment
+  (def day13-layers
+    (input->layers day13-input-str))
+  (trip-severity (layer-scanner-positions-seq day13-layers)))
 
