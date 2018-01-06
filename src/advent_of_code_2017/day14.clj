f09bfa9e42e3200ee11a6b219a19a12c1a28d4c8
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-06 11:31:47 -0500

    Working on Day 14 Part 2

--- src/advent_of_code_2017/day14.clj
+++ src/advent_of_code_2017/day14.clj
@@ -15,13 +15,6 @@
   [prefix]
   (vec (map #(str prefix "-" %) (range 128))))
 
-(def sample-inputs (gen-inputs "flqrgnkx"))
-
-(defn kh-bigint
-  ""
-  [input-str]
-  (BigInteger. (knot/knot-hash input-str) 16))
-
 (defn hexch->binstr
   ""
   [s]
@@ -33,10 +26,51 @@
   [kh]
   (apply str (map #(hexch->binstr (str %)) kh)))
 
-(defn part1
+(defn prefix->binstrs
   ""
   [prefix]
   (let [inputs (gen-inputs prefix)
-        binstrs (map #(knothash->binstr (knot/knot-hash %)) inputs)
-        occupied-slots (apply + (map #(get (frequencies %) \1) binstrs))]
-    occupied-slots))
+        binstrs (map #(knothash->binstr (knot/knot-hash %)) inputs)]
+    binstrs))
+
+(defn part1
+  ""
+  [prefix]
+  (let [binstrs (prefix->binstrs prefix)
+        num-occupied-slots (apply + (map #(get (frequencies %) \1) binstrs))]
+    num-occupied-slots))
+
+(def sample-binstrs (prefix->binstrs "flqrgnkx"))
+
+(def input-binstrs (prefix->binstrs "vbqugkhl"))
+
+;
+; Part 2
+;
+
+; Process one row at a time
+; Record coordinates for slots with a 1
+; Where x coordinates are adjacent, create a region
+; Search previous row's coordinates, where y coordinates are adjacent consolidate regions
+
+(def sample-coords [[1 0 1 0]
+                    [0 1 1 0]
+                    [1 0 0 1]
+                    [0 1 1 0]])
+
+(def binstrs->coords
+  ""
+  [binstrs]
+  (mapv
+    #(mapv (fn [x] (Character/digit x 2)) %)
+    binstrs))
+
+(def occupied-coords
+  ""
+  [coords]
+  (for [x (range (count (first coords)))
+        y (range (count coords))
+        :let [coord [x y]
+              val (get-in coords coord)]
+        :when (= 1 val)]
+    coord))
