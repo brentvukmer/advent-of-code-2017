ed693aea8dfc081644a1c0ebdde907ea042566a1
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-07 16:18:03 -0500

    More work on Day 14 Part 2.  Merging groups into regions..

--- src/advent_of_code_2017/day14.clj
+++ src/advent_of_code_2017/day14.clj
@@ -1,6 +1,6 @@
 (ns advent-of-code-2017.day14
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
+  (:require [clojure.string :as str]
+            [clojure.set :as set]
             [advent-of-code-2017.day10 :as knot]))
 
 ;
@@ -42,78 +42,79 @@
   []
   (count-occupied-slots "vbqugkhl"))
 
-(def sample-binstrs (prefix->binstrs "flqrgnkx"))
+(comment
 
-(def input-binstrs (prefix->binstrs "vbqugkhl"))
+  (def sample-binstrs (prefix->binstrs "flqrgnkx"))
+
+  (def input-binstrs (prefix->binstrs "vbqugkhl")))
 
 ;
 ; Part 2
 ;
-
-; Record coordinates for slots with a 1
-;
-; Process one row at a time
-; Where x coordinates are adjacent, create a region
-; Search previous row's coordinates, where y coordinates are adjacent consolidate regions
 
 (defn binstrs->grid
   ""
   [binstrs]
   (mapv vec binstrs))
 
+(comment
+
+  (def sample-grid (binstrs->grid sample-binstrs))
+
+  (def input-grid (binstrs->grid input-binstrs)))
+
 (defn get-adjacent-coords
   ""
   [coord grid]
-  (let [xs (set (range (count (first grid))))
-        ys (set (range (count grid)))
+  (let [rows (set (range (count grid)))
+        cols (set (range (count (first grid))))
         possible-adj (mapv #(mapv + coord %)
                            [[1 0] [0 1] [-1 0] [0 -1]])]
     (vec
-      (filter (fn [[x y]] (and (contains? xs x)
-                               (contains? ys y))) possible-adj))))
+      (filter (fn [[x y]] (and (contains? rows x)
+                               (contains? cols y)
+                               (= \1 (get-in grid [x y])))) possible-adj))))
 
-(defn discover-regions
+(defn discover-groups
   ""
   [grid]
-  (let [max-x (count (first grid))
-        max-y (count grid)
-        xy-region (atom {:region 0})]
-    (for [x (range max-x)]
-      (for [y (range max-y)
-            :let [coord [x y]
-                  val (get-in grid coord)
-                  current-region (get @xy-region coord)
-                  adjacent-coords (get-adjacent-coords coord grid)
-                  adjacent-region (filter some? (map #(get @xy-region %) adjacent-coords))]
+
+  (vec
+    (let [max-row (count grid)
+          max-col (count (first grid))]
+      (for [row (range max-row)
+            col (range max-col)
+            :let [coord [row col]
+                  val (get-in grid coord)]
             :when (= \1 val)]
-        (cond
+        (set (conj (get-adjacent-coords coord grid) coord))))))
 
-          (and
-            (nil? current-region)
-            (nil? adjacent-region))
+(comment
 
-          (let [new-region (inc (get @xy-region :region))
-                coords (conj adjacent-coords coord)]
-            (do
-              (swap! xy-region assoc :region new-region)
-              (map #(swap! xy-region assoc % new-region) coords)))
+  (def groups1 (discover-groups [[\1 \0 \0 \1 \1 \0]
+                                 [\1 \1 \0 \1 \0 \0]
+                                 [\0 \0 \1 \0 \1 \0]
+                                 [\0 \0 \0 \0 \0 \0]
+                                 [\1 \1 \1 \1 \1 \1]]))
 
-          (and
-            (nil? current-region)
-            (some? adjacent-region))
+  )
 
-          (swap! xy-region assoc coord adjacent-region)
-
-          (and
-            (some? current-region)
-            (nil? adjacent-region))
-
-          (map #(swap! xy-region assoc % current-region) adjacent-coords)
-
-          )
-
-        ))
-    (apply sorted-set (vals @xy-region))))
+(defn merge-groups
+  ""
+  [grid]
+  (loop [groups (discover-groups grid)
+         merged-groups []]
+    (if (empty? groups)
+      merged-groups
+      (let [group (first groups)
+            matching-groups (apply set/union (keep #(when (some group %) %) (rest groups)))
+            merged (set/union matching-groups group)
+            already-merged-matches (apply set/union (keep #(when (some merged %) %) merged-groups))
+            merged-with-already (set/union already-merged-matches merged)
+            pruned (remove #(some merged-with-already %) merged-groups)
+            remaining (keep #(when (nil? (some merged-with-already %)) %) (rest groups))]
+        (recur remaining
+               (conj pruned merged))))))
 
 
 
