d24e271f7407a8db8e8cc91a25f6bef233fa2632
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-24 21:23:28 -0500

    Solved Day 19 Part 1

--- src/advent_of_code_2017/day19.clj
+++ src/advent_of_code_2017/day19.clj
@@ -14,5 +14,43 @@
 ;
 
 (defn read-input
+  ""
   [input-name]
   (clojure.string/split-lines (slurp (io/resource input-name))))
+
+(defn non-space-indexes
+  ""
+  [inputs]
+  (for [i (range (count inputs))
+        :let [line (get inputs i)]]
+    (filter some? (map-indexed (fn [index item] (if (not (Character/isSpace item)) [[i index] item] nil)) line))))
+
+(defn parse-diagram
+  ""
+  [inputs]
+  (->> (non-space-indexes inputs)
+       (apply concat)
+       (into {})))
+
+(defn get-next
+  ""
+  [index remaining-indexes direction]
+  {:next-index [0 0] :next-direction [0 0]})
+
+(defn follow-path
+  ""
+  [path-info]
+  (let [indexes (sort (keys path-info))]
+    (loop [index (first indexes)
+           direction [1 0]
+           letters []
+           remaining-indexes (rest indexes)]
+      (let [c (get path-info index)
+            updated-letters (if (Character/isAlphabetic c) (conj letters c) letters)
+            [next-index next-direction] (get-next index remaining-indexes direction) ]
+        (if (= 0 (second index))
+          updated-letters
+          (recur next-index
+                 next-direction
+                 updated-letters
+                 (remove #(= index %) remaining-indexes)))))))
