ca0e8c8d0074e579e42e465c2bddb3a63a0f2a3a
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-25 19:19:53 -0500

    Solved Day 19 Part 1 and Part 2

--- src/advent_of_code_2017/day19.clj
+++ src/advent_of_code_2017/day19.clj
@@ -32,25 +32,91 @@
        (apply concat)
        (into {})))
 
-(defn get-next
-  ""
-  [index remaining-indexes direction]
-  {:next-index [0 0] :next-direction [0 0]})
 
-(defn follow-path
+(def initial-direction [1 0])
+
+
+(defn remaining-keys
   ""
-  [path-info]
-  (let [indexes (sort (keys path-info))]
-    (loop [index (first indexes)
-           direction [1 0]
-           letters []
-           remaining-indexes (rest indexes)]
-      (let [c (get path-info index)
-            updated-letters (if (Character/isAlphabetic c) (conj letters c) letters)
-            [next-index next-direction] (get-next index remaining-indexes direction) ]
-        (if (= 0 (second index))
-          updated-letters
-          (recur next-index
-                 next-direction
-                 updated-letters
-                 (remove #(= index %) remaining-indexes)))))))
+  [curr keys]
+  (remove #(= curr %) keys))
+
+
+(defn next-using
+  ""
+  [curr direction diagram]
+  (let [direction-next (mapv + curr direction)]
+    (when (contains? diagram direction-next) direction-next)))
+
+
+(defn letter?
+  ""
+  [c]
+  (Character/isAlphabetic (int c)))
+
+
+(defn index-distance
+  ""
+  [index1 index2]
+  (reduce + (mapv #(Math/abs %) (mapv - index1 index2))))
+
+
+(defn find-closest-index
+  [curr remaining-keys]
+  (get-in (vec (sort (group-by #(index-distance curr %) remaining-keys))) [0 1 0]))
+
+
+(defn crossing?
+  ""
+  [c1 c2]
+  (= #{\| \-} (set [c1 c2])))
+
+
+(defn best-direction
+  ""
+  [curr direction next-step best-next]
+  (if (nil? best-next) nil
+                       (if (= next-step best-next)
+                         direction
+                         (mapv - best-next curr))))
+
+
+(defn best-next
+  [curr direction diagram remaining-keys]
+  (let [next-step (next-using curr direction diagram)
+        best-next (if (nil? next-step) (find-closest-index curr remaining-keys) next-step)
+        updated-direction (best-direction curr direction next-step best-next)]
+    [best-next updated-direction]))
+
+
+(defn lazy-path
+  ""
+  ([diagram]
+   (let [path-keys (sort (keys diagram))
+         curr (first path-keys)
+         direction [1 0]
+         seen #{curr}
+         remaining-keys (rest path-keys)]
+     (lazy-path curr direction seen remaining-keys diagram)))
+
+  ([curr direction seen remaining-keys diagram]
+   (let [[next-step next-direction] (best-next curr direction diagram remaining-keys)]
+     (lazy-seq (cons curr (lazy-path next-step next-direction (conj seen next-step) (remove #(= next-step %) remaining-keys) diagram))))))
+
+
+(defn part1
+  ""
+  [diagram]
+  (apply str
+         (filter letter?
+                 (map #(get diagram %)
+                      (take-while some? (lazy-path diagram))))))
+
+;
+; PART 2
+;
+
+(defn part2
+  ""
+  [diagram]
+  (count (take-while some? (lazy-path diagram))))
