36c16e5dfb696345a364c9b9ad17ec0fe6c99452
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-27 12:35:40 -0500

    Solved Day 20 Part 1

--- src/advent_of_code_2017/day20.clj
+++ src/advent_of_code_2017/day20.clj
@@ -25,9 +25,49 @@
             (str/replace "a" ":a"))
         edn-str (str "{" cleaned-up "}")
         xyz (edn/read-string edn-str)]
-     xyz))
+    xyz))
+
+(defn parse-particles-buffer
+  ""
+  [input-name]
+  (let [raw-buffer (read-input input-name)
+        particles (map parse-input-line raw-buffer)
+        indexed-particles (map-indexed (fn [index x] (assoc x :index index)) particles)]
+    indexed-particles))
+
+(defn update-particle
+  "Updates particle velocity by the acceleration, then updates the position using the updated velocity."
+  [particle]
+  (let [{position     :p
+         velocity     :v
+         acceleration :a
+         index        :index} particle
+        updated-velocity (mapv + velocity acceleration)
+        updated-position (mapv + position updated-velocity)]
+    {:p updated-position :v updated-velocity :a acceleration :index index}))
+
+(defn manhattan-distance
+  ""
+  [particle]
+  (let [{position :p} particle]
+    (reduce + (map #(Math/abs %) position))))
+
+(defn find-closest-after
+  ""
+  [n input-name]
+  (:index
+    (first
+      (sort-by #(manhattan-distance %)
+               (last
+                 (take n
+                       (iterate #(map update-particle %)
+                                (parse-particles-buffer input-name))))))))
+
+(defn part1
+  ""
+  []
+  (find-closest-after 1000 "day20"))
 
 ;
 ; Part 2
 ;
-
