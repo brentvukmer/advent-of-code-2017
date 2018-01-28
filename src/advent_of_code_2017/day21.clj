29eedfcbb7e178e1034d890fbfda0c5df32de453
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-28 15:29:41 -0500

    Got enhance working, more or less.

--- src/advent_of_code_2017/day21.clj
+++ src/advent_of_code_2017/day21.clj
@@ -36,14 +36,10 @@
             (io/resource input-name)))))
 
 
-;
-;
-;
 (defn rotate
   "Rotate clockwise by 90 degrees."
   [pixels]
   (apply mapv #(vec (reverse %&)) pixels))
-
 
 
 (defn flip
@@ -51,48 +47,83 @@
   [pixels axis]
   (cond
     (= axis :x)
-    (map #(vec (reverse %)) pixels)
+    (mapv #(vec (reverse %)) pixels)
     (= axis :y)
     (vec (reverse pixels))))
 
-(defn pixels->2by2
-  ""
-  [pixels]
-  [])
 
-; Break the pixels up into 2x2 squares
-; For each 2x2 square:
-; - Find the corresponding enhancement rule (flip or rotate as needed)
-; - Use rule to convert 2x2 square into a 3x3 square
-; Merge 3x3 squares into one
+(defn rule-match-candidates
+  "Create rule-match candidates via rotating and flipping the pixels."
+  [pixels]
+  (set
+    (conj
+      (vec (take 4 (iterate rotate pixels)))
+      (flip pixels :x)
+      (flip pixels :y))))
+
+
+(defn size-matching-rules
+  "Find rules whose :input-size matches the pixels square dimensions."
+  [pattern rules]
+  (filter #(= (count pattern) (:input-size %)) rules))
+
+
+(defn find-matching-rule
+  "Find rule that matches pixel square dimensions and pattern."
+  [pattern rules]
+  (let [candidates (rule-match-candidates pattern)]
+    (first
+      (filter
+        #(contains? candidates (:from %))
+        (size-matching-rules pattern rules)))))
+
+
+(defn pixels->squares
+  "Splits a pixels square into n x n squares."
+  [n pixels]
+  (apply map vector
+         (map #(map vec (partition n %))
+              pixels)))
+
+
 (defn two->three
   ""
-  [pixels]
-  [2 3])
+  [pixels rules]
+  ; Break the pixels up into 2x2 squares
+  (let [squares (pixels->squares 2 pixels)]
+    ; For each 2x2 square:
+    ; - Find the corresponding enhancement rule (flip or rotate as needed)
+    ; - Use rule to convert 2x2 square into a 3x3 square
+    ; Merge 3x3 squares into one
+    squares))
 
 
-; Break the pixels up into 3x3 squares.
-; For each 3x3 square:
-; - Find the corresponding enhancement rule (flip or rotate as needed)
-; - Use rule to convert 3x3 square into a 4x4 square
-; Merge 4x4 squares into one
-(defn three->four
+(defn split-enhance-merge
   ""
-  [pixels]
-  [3 4])
+  [pixels n rules]
+  ; Break the pixels up into n x n squares.
+  ; For each n x n square:
+  ; - Find the corresponding enhancement rule (flip or rotate as needed)
+  ; - Use rule to convert n x n square into an n+1 x n+1 square
+  ; Merge n+1 x n+1 squares into one
+  (let [squares (pixels->squares 3 pixels)
+        transformed (map #(:to (find-matching-rule % rules)) squares)
+        merged (apply map concat transformed)]
+    merged))
 
 
-(defn split-and-convert
+(defn enhance
   ""
-  [pixels]
-  (let [size (count (first pixels))]
-    (cond
-      (= 0 (mod size 2))
-      (two->three pixels)
-      (= 0 (mod size 3))
-      (three->four pixels)
-      :else
-      :error)))
+  [pixels rules]
+  (let [size (count (first pixels))
+        split-factor
+        (cond
+          (= 0 (mod size 2))
+          2
+          (= 0 (mod size 3))
+          3)
+        enhanced (split-enhance-merge pixels split-factor rules)]
+    enhanced))
 
 ;
 ; Part 2
