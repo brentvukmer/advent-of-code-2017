8ff5cef103a50b49b4494afae8a2c48f371e0bb1
Brent Vukmer <brent.vukmer@gmail.com>
2018-01-29 21:00:33 -0500

    Solved Day 21 Part 1

--- src/advent_of_code_2017/day21.clj
+++ src/advent_of_code_2017/day21.clj
@@ -36,15 +36,21 @@
             (io/resource input-name)))))
 
 
-(defn rotate
+(defn rotate-clockwise
   "Rotate clockwise by 90 degrees."
   [pixels]
   (apply mapv #(vec (reverse %&)) pixels))
 
 
+(defn rotations
+  "Rotate clockwise through 360 degrees, then counterclockwise through 360 degrees."
+  [pixels]
+  (set (take 4 (iterate rotate-clockwise pixels))))
+
+
 (defn flip
   "Flip either right-to-left (:x axis) or top-to-bottom (:y axis)."
-  [pixels axis]
+  [axis pixels]
   (cond
     (= axis :x)
     (mapv #(vec (reverse %)) pixels)
@@ -52,14 +58,17 @@
     (vec (reverse pixels))))
 
 
-(defn rule-match-candidates
-  "Create rule-match candidates via rotating and flipping the pixels."
-  [pixels]
-  (set
-    (conj
-      (vec (take 4 (iterate rotate pixels)))
-      (flip pixels :x)
-      (flip pixels :y))))
+(defn rotate-flip-set
+  ""
+  [pattern]
+  (let [rotated (rotations pattern)
+        flip-xs (map #(flip :x %) rotated)
+        flip-ys (map #(flip :y %) rotated)
+        flip-xys (map #(flip :y %) flip-xs)
+        flip-yxs (map #(flip :x %) flip-ys)
+        flips (concat flip-xs flip-ys flip-xys flip-yxs)
+        candidates (into rotated (concat flips (rotations flips)))]
+    candidates))
 
 
 (defn size-matching-rules
@@ -71,61 +80,73 @@
 (defn find-matching-rule
   "Find rule that matches pixel square dimensions and pattern."
   [pattern rules]
-  (let [candidates (rule-match-candidates pattern)]
+  (let [candidates (rotate-flip-set pattern)]
     (first
       (filter
         #(contains? candidates (:from %))
         (size-matching-rules pattern rules)))))
 
-
-(defn pixels->squares
-  "Splits a pixels square into n x n squares."
-  [n pixels]
-  (apply map vector
-         (map #(map vec (partition n %))
-              pixels)))
-
-
-(defn two->three
+(defn split-factor
   ""
-  [pixels rules]
-  ; Break the pixels up into 2x2 squares
-  (let [squares (pixels->squares 2 pixels)]
-    ; For each 2x2 square:
-    ; - Find the corresponding enhancement rule (flip or rotate as needed)
-    ; - Use rule to convert 2x2 square into a 3x3 square
-    ; Merge 3x3 squares into one
-    squares))
-
-
-(defn split-enhance-merge
-  ""
-  [pixels n rules]
-  ; Break the pixels up into n x n squares.
-  ; For each n x n square:
-  ; - Find the corresponding enhancement rule (flip or rotate as needed)
-  ; - Use rule to convert n x n square into an n+1 x n+1 square
-  ; Merge n+1 x n+1 squares into one
-  (let [squares (pixels->squares 3 pixels)
-        transformed (map #(:to (find-matching-rule % rules)) squares)
-        merged (apply map concat transformed)]
-    merged))
-
-
-(defn enhance
-  ""
-  [pixels rules]
-  (let [size (count (first pixels))
+  [c]
+  (let [size (count c)
         split-factor
         (cond
           (= 0 (mod size 2))
           2
           (= 0 (mod size 3))
-          3)
-        enhanced (split-enhance-merge pixels split-factor rules)]
-    enhanced))
+          3)]
+    split-factor))
+
+(defn split
+  "Splits a pixels square into n x n squares."
+  [pixels]
+  ; Account for starting pixels square being divisible by 3 (so already split)
+  (let [n (split-factor pixels)]
+    (if (= n (count pixels))
+      (vector (vector pixels))
+      (mapv
+        #(apply mapv vector
+                (mapv (fn [x]
+                        (map vec (partition n x))) %))
+        (partition n pixels)))))
+
+(defn join
+  "Joins n x n squares back into a single pixels square."
+  [enhanced]
+  (let [n (split-factor enhanced)]
+    ; Account for starting pixels square being divisible by 3 (so already split)
+    (if (= 1 (count enhanced))
+      (first enhanced)
+      (mapv vec
+            (apply concat
+                   (map #(apply map concat %)
+                        (partition n enhanced)))))))
+
+(defn enhance
+  ""
+  [pixels rules]
+  (let [squares (split pixels)
+        enhanced (map #(:to (find-matching-rule % rules)) (apply concat squares))
+        merged-squares (join enhanced)]
+    merged-squares))
+
+
+(defn part1
+  ""
+  [n rules]
+  (count
+    (filter #(= \# %)
+            (flatten
+              (last
+                (take (inc n)
+                      (iterate #(enhance % rules) start-pattern)))))))
+
+
+(comment
+  (part1 2 (parse-rules-from "day21-sample"))
+  (part1 5 (parse-rules-from "day21")))
 
 ;
 ; Part 2
 ;
-
