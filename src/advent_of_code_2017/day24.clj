ba8d374258cb56ed1857f8dc12fa9d24caaf717c
Brent Vukmer <brent.vukmer@gmail.com>
2018-02-11 21:30:05 -0500

    Solved Day 24 Part 1

--- src/advent_of_code_2017/day24.clj
+++ src/advent_of_code_2017/day24.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [clojure.edn :as edn]
-            [clojure.math.combinatorics :as combo]))
+            [clojure.math.combinatorics :as combo]
+            [clojure.set :as set]))
 
 
 ;
@@ -47,23 +48,41 @@
           components)))
 
 
+(defn find-open-port
+  [prev]
+  (let [freqs (frequencies (remove zero? (flatten prev)))
+        available-port (first (filter #(odd? (get freqs %)) (keys freqs)))]
+    available-port))
+
+
+(defn uses-open-port
+  [prev potential-next]
+  (let [open-port (find-open-port prev)]
+    (contains? (set potential-next) open-port)))
+
+
+;
+; This code is gross.  Find a cleaner solution.
+;
 (defn bridges
   ""
   [indexed root]
-  (let [seen (atom #{})]
-    (tree-seq
-      (fn [x] (when-not (@seen (last x))
-                (swap! seen conj (last x))))
-      #(map (fn [t] (conj % t))
-            (filter (fn [y] (not (contains? @seen y)))
-                    (get indexed (last %))))
-      [root])))
+
+  (tree-seq
+    #(some? (seq %))
+    #(map (fn [t] (conj % t))
+          (filter (fn [y] (and
+                            (not (contains? (set %) y))
+                            (uses-open-port % y)))
+                  (get indexed (last %))))
+    [root]))
+
 
 (defn zero-ports
   [components]
-  (let [sets (map set components)
-        zeros (filter #(contains? % 0) sets)]
+  (let [zeros (filter #(contains? (set %) 0) components)]
     (mapv vec zeros)))
+
 
 (defn part1
   [components]
